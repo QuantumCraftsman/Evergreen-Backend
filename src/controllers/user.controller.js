e91a64a125ccd319a422d06a1cf3ea9688d203bf
QuantumCraftsman <rahulgoel03200303@gmail.com>
2025-04-16 12:04:00 +0530

    controller changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,9 +7,60 @@ import { ApiResponse } from "../utils/ApiResponse.js";
  
 
 const registerUser = asyncHandler(async(req,res,next)=>{
-    res.status(200).json({
-        message:"ok"
-    })
+   //get user from frontend
+   //validation - not empty
+   //check if user already exists
+   //check for images ,check for avataar
+   //upload them to cloudinary,avatar
+   //create user object -create enry in db
+
+   const{fullName,email,userName,password} = req.body
+  
+   if([fullName,email,userName,password].some((field)=>
+   field?.trim()==="")
+){
+    throw new ApiError(400,"all feilds are required")
+   }
+    const existedUser = User.findOne({
+    $or: [{userName},{email}]
+   })
+   if(existedUser){
+    throw new ApiError(409,"user with email and username already existes")
+   }
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+   
+    if(!avatarLocalPath ){
+        throw  new ApiError(400,"avattar not found")
+    }
+    if(!coverImageLocalPath){
+        throw  new ApiError(400,"coverimage not found")
+    }
+      const avatar = await  uploadOnCloudinary(avatarLocalPath)
+      const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+      if(!avatar){
+        throw  new ApiError(400,"avater not uploaded to cloudinary")
+      }
+      if(!coverImage){
+        throw  new ApiError(400,"coverimage not uploaded to cloudinary")
+
+      }
+       const user = User.create({
+        fullName,
+        avatar:avatar.url,
+        coverImage: coverImage,
+        email,
+        password,
+        userName:userName.toLowerCase()
+      })
+      const createdUser = await User.findById(user._id).select("-password -refreshToken")
+if(!createdUser){
+    throw  new ApiError(500,"something went wrong while registring the user")
+}
 })
+return res.status(201).json(
+    new ApiResponse(200, createdUser ,"user registered user ")
+)
 
 export {registerUser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,16 +5,17 @@ import { upload } from "../middlewares/multer.middlerware.js";
 const router = Router();
 
 router.route("/register").post(
-    // upload.fields(
-    //     {
-    //         name: "avatar",
-    //         maxCount: 1
-    //     }, 
-    //     {
-    //         name: "coverImage",
-    //         maxCount: 1
-    //     }
-    // ]),
+    upload.fields([{
+        name:"avatar",
+        maxCount:1
+    },
+    {
+        name:"coverImage",
+        maxCount:1
+    }
+
+    ]),
+   
     registerUser
     )
 
