753a1d768983bf7ee13d4a678fcf9251b53bade9
QuantumCraftsman <rahulgoel03200303@gmail.com>
2025-04-16 10:38:19 +0530

    some changes

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use((err, req, res, next) => {
 import userRouter from './routes/user.routes.js'
 
 console.log("route is loaded")
-app.use("api/v1/users",userRouter)
+app.use("/api/v1/users",userRouter)
 
 
 export {app} 
